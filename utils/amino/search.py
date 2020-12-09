702c3e91faea39d3eab3856920989b93c1e39b01
VENOM-InstantDeath <mohathepro111@gmail.com>
2020-12-09 18:37:30 -0300

    Added some files

--- utils/amino/search.py
+++ utils/amino/search.py
@@ -15,8 +15,9 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from tokens import tokenize
+from tokens import tokenize, untoken
 from typing import List
+import json
 
 def compare(x, y):
     coin = 0
@@ -38,25 +39,27 @@ def alg(x: List[str], y: List[str]) -> str:
                     start = (a, b)
     if start == 0:
         raise(RuntimeError)
-    print(comp)
-    print("start",start)
+    z = untoken(x[start[0]], True)
     x = x[start[0]][start[1]:]
-    return x
-
+    comp = compare(untoken(x, True), untoken(y, True))
+    if comp[1] >= 70:
+        return z
+    else:
+        raise RuntimeError
 
 def search(x: List[str]) -> str:
-    #crear listas en memelist
-    memelist = ["abran paso a un par de emprendedores", "ahí viene", "ahora si se viene lo chido", "ah shit here we go again", "algo anda mal", "algo no anda bien en lazy town", "anota eso anota eso", "argentina un país con buena gente", "bueno quién tiene hambre", "cerebrón", "demonios", "diablos señorita", "eres como hitler", "esas preguntas no me dejan dormir", "es el día opuesto", "es el viejo jenkins", "eso no me lo esperaba", "eso tiene sentido para mí", "esta", "hackerman", "hay destinos peores que la muerte", "hola señor turner", "horrified gumball", "i love democracy", "jaja qué dice el boludo de arriba", "komo lo zupo", "las cosas como son", "llamada del circo", "maravillosa jugada", "me da asco solo verlo", "me perturba", "me recordó a mí", "mike sullivan", "mi manera es la manera de los dioses", "miren es mi día de suerte", "mucho texto", "muy sospechoso", "nada no hay no existe", "no confundas las cosas negro", "noice", "no necesito dormir necesito respuestas", "no seas tan crédulo mcfly", "parece que aún tengo aliados en el olimpo", "pero hey las risas no faltaron", "que trucazo no", "quien io", "quita tu cochinada de aquí", "rae", "según mi perapod eso es imposible", "si es real podría valer una fortuna", "soy admin", "soy el sucio dan", "stonks", "te atreves a usar mis propios hechizos", "this is fine", "tu suerte acaba de terminarse", "un clásico", "vivan las mentiras", "ya estoy hasta la puta madre", "yo sólo sé decir de nada"]
-    for i in range(len(memelist)):
-        memelist[i] = tokenize(memelist[i]).full
+    memelist = [['abran', 'paso', 'a', 'un', 'par', 'de', 'emprendedores'], ['ahí', 'viene'], ['ahora', 'si', 'se', 'viene', 'lo', 'chido'], ['ah', 'shit', 'here', 'we', 'go', 'again'], ['algo', 'anda', 'mal'], ['algo', 'no', 'anda', 'bien', 'en', 'lazy', 'town'], ['anota', 'eso', 'anota', 'eso'], ['argentina', 'un', 'país', 'con', 'buena', 'gente'], ['bueno', 'quién', 'tiene', 'hambre'], ['cerebrón'], ['demonios'], ['diablos', 'señorita'], ['eres', 'como', 'hitler'], ['esas', 'preguntas', 'no', 'me', 'dejan', 'dormir'], ['es', 'el', 'día', 'opuesto'], ['es', 'el', 'viejo', 'jenkins'], ['eso', 'no', 'me', 'lo', 'esperaba'], ['eso', 'tiene', 'sentido', 'para', 'mí'], ['esta'], ['hackerman'], ['hay', 'destinos', 'peores', 'que', 'la', 'muerte'], ['hola', 'señor', 'turner'], ['horrified', 'gumball'], ['i', 'love', 'democracy'], ['jaja', 'qué', 'dice', 'el', 'boludo', 'de', 'arriba'], ['komo', 'lo', 'zupo'], ['las', 'cosas', 'como', 'son'], ['llamada', 'del', 'circo'], ['maravillosa', 'jugada'], ['me', 'da', 'asco', 'solo', 'verlo'], ['me', 'perturba'], ['me', 'recordó', 'a', 'mí'], ['mike', 'sullivan'], ['mi', 'manera', 'es', 'la', 'manera', 'de', 'los', 'dioses'], ['miren', 'es', 'mi', 'día', 'de', 'suerte'], ['mucho', 'texto'], ['muy', 'sospechoso'], ['nada', 'no', 'hay', 'no', 'existe'], ['no', 'confundas', 'las', 'cosas', 'negro'], ['noice'], ['no', 'necesito', 'dormir', 'necesito', 'respuestas'], ['no', 'seas', 'tan', 'crédulo', 'mcfly'], ['parece', 'que', 'aún', 'tengo', 'aliados', 'en', 'el', 'olimpo'], ['pero', 'hey', 'las', 'risas', 'no', 'faltaron'], ['qué', 'trucazo', 'no'], ['quien', 'io'], ['quita', 'tu', 'cochinada', 'de', 'aquí'], ['rae'], ['según', 'mi', 'perapod', 'eso', 'es', 'imposible'], ['si', 'es', 'real', 'podría', 'valer', 'una', 'fortuna'], ['soy', 'admin'], ['soy', 'el', 'sucio', 'dan'], ['stonks'], ['te', 'atreves', 'a', 'usar', 'mis', 'propios', 'hechizos'], ['this', 'is', 'fine'], ['tu', 'suerte', 'acaba', 'de', 'terminarse'], ['un', 'clásico'], ['vivan', 'las', 'mentiras'], ['ya', 'estoy', 'hasta', 'la', 'puta', 'madre'], ['yo', 'sólo', 'sé', 'decir', 'de', 'nada']]
+    F = open("memelist.json", "r")
+    d=json.load(F)
     try:
         B=alg(memelist, x)
     except:
         return "Plantilla no encontrada"
-    print(B)
+    return d[B]
 
 if __name__=="__main__":
     x = input(">")
     y=search(tokenize(x).full)
     if y == "Plantilla no encontrada":
         print(False)
+    print(y)
