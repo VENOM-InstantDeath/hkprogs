65f1748aa2e8e6423a4b2b0c4e2c1587173a43a0
VENOM-InstantDeath <mohathepro111@gmail.com>
2020-12-09 18:40:03 -0300

    Updated libs

--- libs/python/tokens/tokens.py
+++ libs/python/tokens/tokens.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 
+#   This file is part of hkprogs
+
+#   hkprogs is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+
+#   hkprogs is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from typing import List
 
 class obj:
@@ -11,7 +26,12 @@ class obj:
         self.size = 0
         self.full = []
 
-def untoken(X: List) -> str:
+def untoken(X: List, Y=False) -> str:
+    if Y:
+        s = ""
+        for i in X:
+            s += "{} ".format(str(i))
+        return s.strip()
     s = ""
     for i in X:
         s += str(i)
